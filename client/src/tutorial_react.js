ee3e1acd9c8c9f16d3c8e8251287d15b9dd10459
MasterMunix <ron.reyes12@hotmail.com>
2022-09-30 22:17:27 -0400

    Keeping up with the React tutorial till choosing a key

--- client/src/tutorial_react.js
+++ client/src/tutorial_react.js
@@ -1,43 +1,39 @@
-import React from 'react';
+import React from "react";
 
-
-class Square extends React.Component {
 /***
- * Como un siguiente paso, queremos que el componente Square “recuerde” 
- * que fue clickeado, y se rellene con una marca de “X”. 
+ * Como un siguiente paso, queremos que el componente Square “recuerde”
+ * que fue clickeado, y se rellene con una marca de “X”.
  * Para “recordar” cosas, los componentes usan estado.
  * Primero, vamos a agregar el constructor a la clase para inicializar el estado:
  */
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '0',
-    };
-  }
 
-  render() {
-    return (
-      <button 
-      className="square" 
-      onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
-      </button>
-    );
-  }
+// constructor(props) {
+//   super(props);
+//   this.state = {       //  ** Lo elimine porque no sigue el estado del componente Square**
+//     value: '0',
+//   };
+// }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -59,15 +55,70 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares,
+        },
+      ]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    /**Usando el método map, podemos mapear nuestro historial de movimientos a elementos de React representando botones en la pantalla, y mostrando una lista de botones para “saltar” a movimientos anteriores.
+
+Vamos a mapear sobre el historial en el método render del componente Game: */
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares = {current.squares}
+            onClick = {(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
@@ -77,3 +128,24 @@ class Game extends React.Component {
 // ========================================
 
 export default Game;
+
+//function to calculate the winner in the game
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
